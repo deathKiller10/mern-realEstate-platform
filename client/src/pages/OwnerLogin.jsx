031a7803159dd5b0c122e2591046976c8fa9b738
deathKiller10 <priyanshuwork.10@gmail.com>
2026-03-02 00:00:28 +0530

    Frontend work correctly saved in client folder

---
+++ client/src/pages/OwnerLogin.jsx
@@ -0,0 +1,88 @@
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/Authcontext";
+import { NavLink, useNavigate } from "react-router-dom";
+
+function OwnerLogin() {
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();  
+  const [formdata, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (d) => {
+    setForm({
+      ...formdata,
+      [d.target.name]: d.target.value,
+    });
+  };
+
+  const validatechange = () => {
+    const { email, password } = formdata;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^[A-Za-z0-9]{8,}$/;
+
+    if (!emailRegex.test(email)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+
+    if (!passwordRegex.test(password)) {
+      alert("Password must be at least 8 characters and contain only letters (A-Z, a-z) and numbers (0-9). No special characters allowed.");
+      return;
+    }
+
+    login(email);
+    alert("Login Successful!");
+    navigate("/ownerdetails");
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-blue-900 bg-opacity-90">
+
+      <div className="bg-white p-10 rounded-lg shadow-lg w-96">
+
+        <h2 className="text-2xl font-bold text-center text-blue-900 mb-6">
+          Property Owner Login
+        </h2>
+
+        <input
+          type="text"
+          name="email"
+          placeholder="Email Id"
+          onChange={handleChange}
+          className="w-full border p-3 mb-4 rounded focus:outline-none focus:ring-2 focus:ring-green-400 "
+        /><br></br><br></br>
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          className="w-full border p-3 mb-4 rounded focus:outline-none focus:ring-2 focus:ring-green-400"
+        /><br></br><br></br>
+
+        <button
+          onClick={validatechange}
+          className="w-full bg-green-500 hover:bg-green-600 text-white py-3 rounded"
+        >
+          Login
+        </button>
+
+        <p className="text-center mt-4">
+          Don't have an account?{" "}
+          <NavLink
+            to="/ownerregister"
+            className="text-green-500 font-medium"
+          >
+            Create New Account
+          </NavLink>
+        </p>
+
+      </div>
+    </div>
+  );
+}
+
+export default OwnerLogin;
---
+++ client/src/router.jsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import BuyerLogin from "./pages/BuyerLogin";
+import OwnerLogin from "./pages/OwnerLogin";
+
+import Login from "./pages/Login";
+import BuyerRegister from "./pages/BuyerRegister";
+import OwnerRegister from "./pages/OwnerRegister";
+import OwnerDetails from "./pages/OwnerDetails";
+//import OwnerProtectedRoute from "./pages/OwnerProtectedRoute";
+function Layout() {
+  return (
+    <div>
+        <Header />
+        <div style={{ minHeight: "70vh", padding: "20px" }}><Outlet /></div>
+        <Footer /> 
+    </div>
+  );
+}
+
+/* ===== Router ===== */
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      { index: true},
+      { path: "about", Component: About },
+      { path: "login", Component: Login },
+      { path: "buyerlogin", Component: BuyerLogin },
+      { path: "ownerlogin", Component: OwnerLogin },
+      { path: "buyerregister", Component: BuyerRegister },
+      { path: "ownerregister", Component: OwnerRegister },
+      {path:"ownerdetails", Component:OwnerDetails},
+      //{path:"ownerprotectedroute", Component:OwnerProtectedRoute},
+    ],
+  },
+]);
